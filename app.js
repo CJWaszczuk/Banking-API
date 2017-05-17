ea8d6510f9229fbdac1e851bad41f21c2a08e604
cjwaszczuk <waszczukc@parliament.uk>
2017-05-17 09:10:38 +0100

    updated app.js

--- app.js
+++ app.js
@@ -54,3 +54,62 @@ app.get('/mongo', function (req,res){
 })
 
 app.listen(3002);
+
+db.bank_details.insert([{
+	bank_name: 'Allied Irish Bank',
+	atm_url_available: false,
+	atm_url: 'https://api.aibgb.co.uk/open-banking/v1.2/atms'
+},{
+	bank_name: 'Bank of Ireland',
+	atm_url_available: true,
+	atm_url: 'https://openapi.bankofireland.com/open-banking/v1.2/atms'
+},{
+	bank_name: 'Bank of Scotland',
+	atm_url_available: true,
+	atm_url: 'https://api.bankofscotland.co.uk/open-banking/v1.2/atms'
+},{
+	bank_name: 'Barclays Bank',
+	atm_url_available: true,
+	atm_url: 'https://atlas.api.barclays/open-banking/v1.3/atms'
+},{
+	bank_name: 'Danske Bank',
+	atm_url_available: true,
+	atm_url: 'https://obp-api.danskebank.com/open-banking/v1.2/atms'
+},{
+	bank_name: 'First Trust Bank ',
+	atm_url_available: true,
+	atm_url: 'https://api.firsttrustbank.co.uk/open-banking/v1.2/atms'
+},{
+	bank_name: 'Halifax',
+	atm_url_available: true,
+	atm_url: 'https://api.halifax.co.uk/open-banking/v1.2/atms'
+},{
+	bank_name: 'HSBC Group',
+	atm_url_available: true,
+	atm_url: 'https://api.hsbc.com/open-banking/v1.2/atms'
+},{
+	bank_name: 'Lloyds Bank',
+	atm_url_available: true,
+	atm_url: 'https://api.lloydsbank.com/open-banking/v1.2/atms'
+},{
+	bank_name: 'Nationwide Building Society',
+	atm_url_available: true,
+	atm_url: 'https://openapi.nationwide.co.uk/open-banking/v1.2/atms'
+},{
+	bank_name: 'Natwest',
+	atm_url_available: true,
+	atm_url: 'https://openapi.natwest.com/open-banking/v1.2/atms'
+},{
+	bank_name: 'Royal Bank of Scotland',
+	atm_url_available: true,
+	atm_url: 'https://openapi.rbs.co.uk/open-banking/v1.2/atms'
+},{
+	bank_name: 'Santander UK',
+	atm_url_available: true,
+	atm_url: 'https://api.santander.co.uk/retail/open-banking/v1.2/atms'
+},{
+	bank_name: 'Ulster Bank',
+	atm_url_available: true,
+	atm_url: 'https://openapi.ulsterbank.co.uk/open-banking/v1.2/atms'
+}]
+)
